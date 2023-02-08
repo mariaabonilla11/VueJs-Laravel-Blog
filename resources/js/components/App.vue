b993051fe16894b22b63a0f3d945b50ab76e7b1a
mariaab <mariaabonilla11@hotmail.com>
2023-02-08 17:59:54 -0500

    First Commit

---
+++ resources/js/components/App.vue
@@ -0,0 +1,44 @@
+<template>
+    <main>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/9/95/Vue.js_Logo_2.svg" alt="" width="30" height="24">
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <router-link exact-active-class="active" to="/" class="nav-link active" aria-current="page">Inicio</router-link>
+                        </li>
+                        <li class="nav-item">
+                            <router-link exact-active-class="active" to="/blogs" class="nav-link">Blogs</router-link>
+                        </li>
+                        <li class="nav-item">
+                            <router-link exact-active-class="active" to="/contacto" class="nav-link">Contacto</router-link>
+                        </li>
+                    </ul>
+                <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+                </div>
+
+            </div>
+        </nav>
+
+
+
+        <div>
+            <router-view></router-view>
+        </div>
+    </main>
+</template>
+
+<script>
+</script>
---
+++ resources/js/routes.js
@@ -0,0 +1,35 @@
+const Home = () => import('./components/Home.vue')
+const Contacto = () => import('./components/Contacto.vue')
+
+
+const Mostrar = () => import('./components/blog/Mostrar.vue')
+const Crear = () => import('./components/blog/Crear.vue')
+const Editar = () => import('./components/blog/Editar.vue')
+
+export const routes = [
+    {
+        name: 'home',
+        path: '/',
+        component: Home
+    },
+    {
+        name: 'contacto',
+        path: '/contacto',
+        component: Contacto
+    },
+    {
+        name: 'mostrarBlogs',
+        path: '/blogs',
+        component: Mostrar
+    },
+    {
+        name: 'editarBlog',
+        path: '/editar/:id',
+        component: Editar
+    },
+    {
+        name: 'crearBlog',
+        path: '/crear',
+        component: Crear
+    },
+];
